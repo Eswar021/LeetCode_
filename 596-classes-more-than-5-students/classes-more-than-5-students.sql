2f7fe3ecb53dd78a6c0d24ae23501e8d8dc29e5f
Eswar021 <118106134+Eswar021@users.noreply.github.com>
2023-09-15 07:07:45 +0530

    Time: 480 ms (99.08%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-more-than-5-students/classes-more-than-5-students.sql
+++ 596-classes-more-than-5-students/classes-more-than-5-students.sql
@@ -1,1 +1,10 @@
-select class from (select class,count(student) as cnt from courses group by class) as new where new.cnt>=5
+# select class from (select class,count(student) as cnt from courses group by class) as new where new.cnt>=5
+# --------------
+# select class from(
+# select class,count(student) as std from courses
+# group by class
+# having std>=5) as new
+# --------------
+select class from courses
+group by class
+having count(student)>=5
