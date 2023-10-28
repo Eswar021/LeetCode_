ebc1c51fd41de4530afec36eb4974e38f6e273f5
Eswar021 <118106134+Eswar021@users.noreply.github.com>
2023-10-28 23:09:28 +0530

    Time: 50 ms (14.41%) | Memory: 16.2 MB (87.26%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,17 +5,30 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        L=[]
-        while list1 is not None:
-            L.append(list1.val)
-            list1=list1.next
-        while list2 is not None:
-            L.append(list2.val)
-            list2=list2.next
-        L.sort()
-        dummy=ListNode()
-        head=dummy
-        for i in L:
-            dummy.next=ListNode(i)
-            dummy=dummy.next
-        return head.next
+        # L=[]
+        # while list1 is not None:
+        #     L.append(list1.val)
+        #     list1=list1.next
+        # while list2 is not None:
+        #     L.append(list2.val)
+        #     list2=list2.next
+        # L.sort()
+        # dummy=ListNode()
+        # head=dummy
+        # for i in L:
+        #     dummy.next=ListNode(i)
+        #     dummy=dummy.next
+        # return head.next
+        cur=dummy=ListNode()
+        while list1 and list2:
+            if list1.val<list2.val:
+                cur.next=list1
+                list1=list1.next
+                cur=cur.next
+            else:
+                cur.next=list2
+                list2=list2.next
+                cur=cur.next
+        if list1 or list2:
+            cur.next=list1 if list1 else list2
+        return dummy.next
