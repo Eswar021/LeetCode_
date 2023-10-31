c0628f72d3474f9b816ce7323e381bc80d0a00d8
Eswar021 <118106134+Eswar021@users.noreply.github.com>
2023-10-31 14:53:23 +0530

    Time: 50 ms (77.57%) | Memory: 16.4 MB (35.30%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        s = defaultdict(int)
+        # if len()
+        for i in nums1:
+            s[i] +=1
+        ans = []
+        for i in nums2:
+            if s[i]>0:
+                ans.append(i)
+                s[i] -=1
+
+        return ans
