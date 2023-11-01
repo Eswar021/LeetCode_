1ef6467ad3204fea7b3eaebd104bf04d18a17d53
Eswar021 <118106134+Eswar021@users.noreply.github.com>
2023-11-01 10:51:44 +0530

    Time: 53 ms (6.50%) | Memory: 16.3 MB (22.57%) - LeetSync

--- 61-rotate-list/rotate-list.py
+++ 61-rotate-list/rotate-list.py
@@ -7,43 +7,31 @@ class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         len=0
         a=head
-        while a:
-            a=a.next
-            len+=1
-        if len<=k and len!=0:
-            n=k%len
-            for i in range(n):
-                curr=head
-                while curr.next.next:
-                    curr=curr.next
-                temp=curr.next
-                curr.next=None
-                temp.next=head
-                head=temp
-        
-        elif len!=0:
-            for i in range(k):
-                curr=head
-                # if len!=0:
-                while curr.next.next:
-                    curr=curr.next
-                temp=curr.next
-                curr.next=None
-                temp.next=head
-                head=temp
+        if head:
+            while a:
+                a=a.next
+                len+=1
+            if len <= k:
+                for i in range(k%len):
+                    cur=head
+                    while cur.next.next:
+                        cur=cur.next
+                    next=cur.next
+                    cur.next=None
+                    next.next=head
+                    head=next
+            else:
+                for i in range(k):
+                    cur=head
+                    while cur.next.next:
+                        cur=cur.next
+                    next=cur.next
+                    cur.next=None
+                    next.next=head
+                    head=next
+        else:
+            return head
         return head
-        # for i in range(n):
-        #     curr=head
-        #     try:
-        #         while curr.next.next:
-        #             curr=curr.next
-        #         temp=curr.next
-        #         curr.next=None
-        #         temp.next=head
-        #         head=temp
-        #     except:
-        #         pass
-       
 
 
 
