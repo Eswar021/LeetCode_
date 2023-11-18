31fdc3e244f6409344e2d56f2559a5dcc4576d73
Eswar021 <118106134+Eswar021@users.noreply.github.com>
2023-11-18 20:39:07 +0530

    Time: 1355 ms (5.04%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,9 +1,8 @@
+select email from person
+group by email 
+having count(email)>1;
 # Write your MySQL query statement below
 # select new.email as Email from (select email,count(email) as cou from person
 # group by email)as new
 # where cou>=2
 
-
-select email from person
-group by email 
-having count(email)>1
